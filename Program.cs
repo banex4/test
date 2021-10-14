e22f037def1b9868c3077e93ce95c67b95713de3
banex4 <83065097+banex4@users.noreply.github.com>
2021-10-14 22:23:19 +0200

    Add files via upload
    hello world

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrvniAplikace
+{
+
+    /*
+     *	       __          __                __            
+     *	  ____/ /__ _   __/ /_  ____  ____  / /__ _________
+     *	 / __  / _ \ | / / __ \/ __ \/ __ \/ //_// ___/_  /
+     *	/ /_/ /  __/ |/ / /_/ / /_/ / /_/ / ,< _/ /__  / /_
+     *	\__,_/\___/|___/_.___/\____/\____/_/|_(_)___/ /___/
+     *                                                   
+     *                                                           
+     *      TUTORIÁLY  <>  DISKUZE  <>  KOMUNITA  <>  SOFTWARE
+     * 
+     *	Tento zdrojový kód je součástí tutoriálů na programátorské 
+     *	sociální síti DEVBOOK.CZ	
+     *	
+     *	Kód můžete upravovat pod licencí MIT, 
+     *	tedy jak chcete, jen zmiňte odkaz na www.devbook.cz :-) 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello devbook!");
+            Console.ReadKey();
+        }
+    }
+}
